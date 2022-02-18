048457c026023c3e245e78331ec839545746f9a4
tcfplayz <chau123ka89chun10p@gmail.com>
2022-02-18 09:05:52 +0800

    notch

--- src/main/java/notch/launcher/Loader.java
+++ src/main/java/notch/launcher/Loader.java
@@ -1,7 +1,13 @@
 package notch.launcher;
 
+import net.arikia.dev.drpc.DiscordRPC;
+import notch.launcher.discord.DiscordRP;
+
 public class Loader {
 
     public static void main(String[] args) {
+        DiscordRP dcrp = new DiscordRP();
+        dcrp.start();
+        dcrp.update("test a", "test b", "notch", java.util.Optional.of("space"));
     }
 }
--- src/main/java/notch/launcher/discord/DiscordRP.java
+++ src/main/java/notch/launcher/discord/DiscordRP.java
@@ -1,4 +1,54 @@
 package notch.launcher.discord;
 
+import net.arikia.dev.drpc.DiscordEventHandlers;
+import net.arikia.dev.drpc.DiscordRPC;
+import net.arikia.dev.drpc.DiscordRichPresence;
+import net.arikia.dev.drpc.DiscordUser;
+import net.arikia.dev.drpc.callbacks.ReadyCallback;
+
+import java.util.Optional;
+
 public class DiscordRP {
+
+    public boolean running = true;
+    public long runtime = 0;
+
+    public void update(String linea, String lineb, String imga, Optional<String> imgb) {
+        DiscordRichPresence.Builder b = new DiscordRichPresence.Builder(lineb);
+        b.setBigImage(imga, imga);
+        b.setStartTimestamps(runtime);
+        b.setDetails(linea);
+        if (imgb.isPresent()) {
+            b.setSmallImage(imgb.get(), imgb.get());
+        }
+        DiscordRPC.discordUpdatePresence(b.build());
+    }
+
+    public void start() {
+        this.runtime = System.currentTimeMillis();
+        DiscordEventHandlers handlers = new DiscordEventHandlers.Builder().setReadyEventHandler(new ReadyCallback() {
+
+            @Override
+            public void apply(DiscordUser discordUser) {
+                System.out.println("Discord ready!");
+            }
+        }).build();
+
+        DiscordRPC.discordInitialize("804355313460969523", handlers, true);
+
+        new Thread("Discord") {
+            @Override
+            public void run() {
+                while (running) {
+                    DiscordRPC.discordRunCallbacks();
+                }
+            }
+        }.start();
+    }
+
+    public void stop() {
+        running = false;
+        DiscordRPC.discordShutdown();
+    }
+
 }
